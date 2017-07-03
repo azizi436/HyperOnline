145e525af3d0c9a2a0120ec83b48474556de0a01
Arash Hatami <hatamiarash7@gmail.com>
2017-07-03 22:27:11 +0430

    Update Application
    add libraries
    remove tests
    add pushe
    complete gradle settings

--- app/src/main/java/ir/hatamiarash/hyperonline/MainActivity.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/MainActivity.java
@@ -1,7 +1,9 @@
 package ir.hatamiarash.hyperonline;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import co.ronash.pushe.Pushe;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Pushe.initialize(this, true);
     }
 }
