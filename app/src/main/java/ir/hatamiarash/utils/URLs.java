5b9bf8557f4ae06deaae84db767068d7a5b52556
Arash Hatami <hatamiarash7@gmail.com>
2017-11-02 13:48:11 +0330

    change urls

--- app/src/main/java/ir/hatamiarash/utils/URLs.java
+++ app/src/main/java/ir/hatamiarash/utils/URLs.java
@@ -5,15 +5,9 @@
 package ir.hatamiarash.utils;
 
 public class URLs {
-    private static final String IP = "hyper-online.ir";
-//    private static final String IP = "192.168.1.104";
-    private static final String API = "api/v1/";
-    public static final String MAIN = "http://" + IP + "/";
-    public static final String base_URL = "http://" + IP + "/" + API;
-    public static final String image_URL = "http://" + IP + "/images/";
-    public static final String factor_URL = "http://" + IP + "/ftp/factors/";
-    
-    public static final String Check_URL = "https://pardano.com/p/mobilepayment/" + TAGs.API_KEY;
-    public static final String Pay_URL = "https://pardano.com/p/payment/";
-    public static final String Verify_URL = "https://pardano.com/p/mobileverify/";
+    private static final String HOST = "hyper-online.ir";
+    //    private static final String IP = "192.168.1.104";
+    public static final String base_URL = "http://" + HOST + "/api/v1/";
+    public static final String image_URL = "http://" + HOST + "/images/";
+    public static final String factor_URL = "http://" + HOST + "/ftp/factors/";
 }
