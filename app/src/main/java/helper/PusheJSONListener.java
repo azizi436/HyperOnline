d47db935da636e96cc26d3b3f585aac5329cb0cb
Arash Hatami <hatamiarash7@gmail.com>
2017-11-03 18:18:45 +0330

    add force logout

--- app/src/main/java/helper/PusheJSONListener.java
+++ app/src/main/java/helper/PusheJSONListener.java
@@ -13,19 +13,39 @@ import co.ronash.pushe.PusheListenerService;
 
 public class PusheJSONListener extends PusheListenerService {
     SQLiteHandlerSupport db_support;
+    SQLiteHandler db_user;
+    SQLiteHandlerItem db_item;
+    SessionManager session;
+    ConfirmManager confirmManager;
     
     @Override
     public void onMessageReceived(final JSONObject message, JSONObject content) {
         if (message != null && message.length() > 0) {
             db_support = new SQLiteHandlerSupport(getApplicationContext());
+            db_user = new SQLiteHandler(getApplicationContext());
+            db_item = new SQLiteHandlerItem(getApplicationContext());
+            session = new SessionManager(getApplicationContext());
+            confirmManager = new ConfirmManager(getApplicationContext());
             Log.i("Pushe", "Message: " + message.toString());
             try {
-                JSONObject msg = message.getJSONObject("msg");
-                db_support.AddMessage(
-                        msg.getString("title"),
-                        msg.getString("body"),
-                        msg.getString("date")
-                );
+                if (message.has("msg")) {
+                    JSONObject msg = message.getJSONObject("msg");
+                    db_support.AddMessage(
+                            msg.getString("title"),
+                            msg.getString("body"),
+                            msg.getString("date")
+                    );
+                } else if (message.has("logout")) {
+                    JSONObject msg = message.getJSONObject("logout");
+                    if (msg.getBoolean("out")) {
+                        session.setLogin(false);
+                        confirmManager.setPhoneConfirm(false);
+                        confirmManager.setInfoConfirm(false);
+                        db_user.deleteUsers();
+                        db_item.deleteItems();
+                        db_support.deleteMessages();
+                    }
+                }
             } catch (JSONException e) {
                 Log.e("Pushe", "Exception : ", e);
             }
