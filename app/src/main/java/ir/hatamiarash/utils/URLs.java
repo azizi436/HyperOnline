2ff8d2ab8d59ba093e0a7a6c1596ed77683851db
Arash Hatami <hatamiarash7@gmail.com>
2017-12-24 16:20:37 +0330

    add share urls

--- app/src/main/java/ir/hatamiarash/utils/URLs.java
+++ app/src/main/java/ir/hatamiarash/utils/URLs.java
@@ -3,11 +3,17 @@
  */
 
 package ir.hatamiarash.utils;
- 
+
 public class URLs {
     private static final String HOST = "hyper-online.ir";
-//        private static final String HOST = "192.168.1.104";
+    //    public static final String HOST = "192.168.1.104";
     public static final String base_URL = "http://" + HOST + "/api/v1/";
     public static final String image_URL = "http://" + HOST + "/images/";
     public static final String factor_URL = "http://" + HOST + "/ftp/factors/";
+    public static final String pay_URL = "http://" + HOST + "/android/pay/";
+    
+    private static final String Share_CafeBazaar = "https://cafebazaar.ir/app/ir.hatamiarash.hyperonline/?l=fa";
+    private static final String Share_IranApps = "http://iranapps.ir/app/ir.hatamiarash.hyperonline";
+    public static final String Share_App = Share_IranApps;
+    
 }
