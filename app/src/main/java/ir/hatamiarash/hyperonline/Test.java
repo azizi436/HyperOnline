f24dd3c7be8f8f516dc398df5df0ff783b546191
Arash Hatami <hatamiarash7@gmail.com>
2018-01-18 17:07:18 +0330

    remove test content

--- app/src/main/java/ir/hatamiarash/hyperonline/Test.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/Test.java
@@ -4,40 +4,14 @@
 
 package ir.hatamiarash.hyperonline;
 
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.LinearInterpolator;
-import android.widget.ImageView;
 
 public class Test extends AppCompatActivity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.test);
-        
-        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
-        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
-        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
-        
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(10000L);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                final float progress = (float) animation.getAnimatedValue();
-                final float width = backgroundOne.getWidth();
-                final float height = backgroundOne.getHeight();
-                final float translationX = width * progress;
-                final float translationY = height * progress;
-                //backgroundOne.setTranslationX(translationX);
-                //backgroundTwo.setTranslationX(translationX - width);
-                backgroundOne.setTranslationY(translationY);
-                backgroundTwo.setTranslationY(translationY - height);
-            }
-        });
-        animator.start();
     }
 }
 
