5617c22f3ec792a07f088a81b3ef2d6a976fe89f
hatamiarash7 <hatamiarash7@gmail.com>
2018-02-13 23:32:57 +0330

    add server's ip
    Signed-off-by: hatamiarash7 <hatamiarash7@gmail.com>

--- app/src/main/java/ir/hatamiarash/hyperonline/Activity_Splash.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/Activity_Splash.java
@@ -29,101 +29,98 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import helper.FontHelper;
 import helper.Helper;
+import ir.hatamiarash.utils.URLs;
 
 public class Activity_Splash extends AppCompatActivity {
-    @BindView(R.id.logo)
-    public ImageView logo;
-    @BindView(R.id.spinner)
-    public ProgressBar spinner;
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        ButterKnife.bind(this);
-        
-        Wave animation = new Wave();
-        spinner.setIndeterminateDrawable(animation);
-        logo.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.logo));
-        CheckConnection();
-//        Intent i = new Intent(Activity_Splash.this, Activity_Main.class);
-//        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        startActivity(i);
-//        finish();
-    }
-    
-    private void CheckConnection() {
-        if (Helper.CheckInternet2(getApplicationContext())) {
-            new CheckInternet().execute("http://hyper-online.ir/api/200");
-        } else {
-            new MaterialStyledDialog.Builder(Activity_Splash.this)
-                    .setTitle(FontHelper.getSpannedString(getApplicationContext(), "خطا"))
-                    .setDescription(FontHelper.getSpannedString(getApplicationContext(), "اتصال به اینترنت را بررسی نمایید."))
-                    .setStyle(Style.HEADER_WITH_TITLE)
-                    .setHeaderColor(R.color.Red)
-                    .withDarkerOverlay(true)
-                    .withDialogAnimation(true)
-                    .setCancelable(false)
-                    .setPositiveText("باشه")
-                    .setNegativeText("باشه")
-                    .onPositive(new MaterialDialog.SingleButtonCallback() {
-                        @Override
-                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                            CheckConnection();
-                        }
-                    })
-                    .onNegative(new MaterialDialog.SingleButtonCallback() {
-                        @Override
-                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                            android.os.Process.killProcess(android.os.Process.myPid());
-                        }
-                    })
-                    .show();
-        }
-    }
-    
-    class CheckInternet extends AsyncTask<String, Void, Boolean> {
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) new URL(strings[0]).openConnection();
-                connection.setConnectTimeout(2000);
-                return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
-            } catch (SocketTimeoutException | MalformedURLException e) {
-                return false;
-            } catch (IOException e) {
-                return false;
-            } catch (Exception e) {
-                return false;
-            }
-        }
-        
-        @Override
-        protected void onPostExecute(Boolean result) {
-            super.onPostExecute(result);
-            if (result) {
-                Intent i = new Intent(Activity_Splash.this, Activity_Main.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i);
-                finish();
-            } else {
-                new MaterialStyledDialog.Builder(Activity_Splash.this)
-                        .setTitle(FontHelper.getSpannedString(getApplicationContext(), "خطا"))
-                        .setDescription(FontHelper.getSpannedString(getApplicationContext(), "اتصال به اینترنت را بررسی نمایید"))
-                        .setStyle(Style.HEADER_WITH_TITLE)
-                        .setHeaderColor(R.color.Red)
-                        .withDarkerOverlay(true)
-                        .withDialogAnimation(true)
-                        .setCancelable(false)
-                        .setPositiveText("باشه")
-                        .onPositive(new MaterialDialog.SingleButtonCallback() {
-                            @Override
-                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                CheckConnection();
-                            }
-                        })
-                        .show();
-            }
-        }
-    }
+	@BindView(R.id.logo)
+	public ImageView logo;
+	@BindView(R.id.spinner)
+	public ProgressBar spinner;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		ButterKnife.bind(this);
+		
+		Wave animation = new Wave();
+		spinner.setIndeterminateDrawable(animation);
+		logo.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.logo));
+		CheckConnection();
+	}
+	
+	private void CheckConnection() {
+		if (Helper.CheckInternet2(getApplicationContext())) {
+			new CheckInternet().execute("http://" + URLs.IP + "/api/200");
+		} else {
+			new MaterialStyledDialog.Builder(Activity_Splash.this)
+					.setTitle(FontHelper.getSpannedString(getApplicationContext(), "خطا"))
+					.setDescription(FontHelper.getSpannedString(getApplicationContext(), "اتصال به اینترنت را بررسی نمایید."))
+					.setStyle(Style.HEADER_WITH_TITLE)
+					.setHeaderColor(R.color.Red)
+					.withDarkerOverlay(true)
+					.withDialogAnimation(true)
+					.setCancelable(false)
+					.setPositiveText("باشه")
+					.setNegativeText("باشه")
+					.onPositive(new MaterialDialog.SingleButtonCallback() {
+						@Override
+						public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+							CheckConnection();
+						}
+					})
+					.onNegative(new MaterialDialog.SingleButtonCallback() {
+						@Override
+						public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+							android.os.Process.killProcess(android.os.Process.myPid());
+						}
+					})
+					.show();
+		}
+	}
+	
+	class CheckInternet extends AsyncTask<String, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(String... strings) {
+			try {
+				HttpURLConnection connection = (HttpURLConnection) new URL(strings[0]).openConnection();
+				connection.setConnectTimeout(2000);
+				return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+			} catch (SocketTimeoutException | MalformedURLException e) {
+				return false;
+			} catch (IOException e) {
+				return false;
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			super.onPostExecute(result);
+			if (result) {
+				Intent i = new Intent(Activity_Splash.this, Activity_Main.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(i);
+				finish();
+			} else {
+				new MaterialStyledDialog.Builder(Activity_Splash.this)
+						.setTitle(FontHelper.getSpannedString(getApplicationContext(), "خطا"))
+						.setDescription(FontHelper.getSpannedString(getApplicationContext(), "اتصال به اینترنت را بررسی نمایید"))
+						.setStyle(Style.HEADER_WITH_TITLE)
+						.setHeaderColor(R.color.Red)
+						.withDarkerOverlay(true)
+						.withDialogAnimation(true)
+						.setCancelable(false)
+						.setPositiveText("باشه")
+						.onPositive(new MaterialDialog.SingleButtonCallback() {
+							@Override
+							public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+								CheckConnection();
+							}
+						})
+						.show();
+			}
+		}
+	}
 }
--- app/src/main/java/ir/hatamiarash/utils/URLs.java
+++ app/src/main/java/ir/hatamiarash/utils/URLs.java
@@ -5,17 +5,18 @@
 package ir.hatamiarash.utils;
 
 public class URLs {
-        private static final String HOST = "hyper-online.ir";
-//    private static final String HOST = "192.168.1.104";
-    public static final String base_URL = "http://" + HOST + "/api/v1/";
-    public static final String image_URL = "http://" + HOST + "/images/";
-    public static final String factor_URL = "http://" + HOST + "/ftp/factors/";
-    public static final String pay_URL = "http://" + HOST + "/android/pay/";
-    
-    private static final String Share_CafeBazaar = "https://cafebazaar.ir/app/ir.hatamiarash.hyperonline/?l=fa";
-    private static final String Share_IranApps = "http://iranapps.ir/app/ir.hatamiarash.hyperonline";
-    private static final String Share_Myket = "http://myket.ir/app/ir.hatamiarash.hyperonline";
-    private static final String Share_Normal = "http://hyper-online.ir/HyperOnline.apk";
-    public static final String Share_App = Share_Myket;
-    
+	public static final String IP = "185.105.186.135";
+	private static final String HOST = "hyper-online.ir";
+	//    private static final String HOST = "192.168.1.104";
+	public static final String base_URL = "http://" + HOST + "/api/v1/";
+	public static final String image_URL = "http://" + HOST + "/images/";
+	public static final String factor_URL = "http://" + HOST + "/ftp/factors/";
+	public static final String pay_URL = "http://" + HOST + "/android/pay/";
+	
+	private static final String Share_CafeBazaar = "https://cafebazaar.ir/app/ir.hatamiarash.hyperonline/?l=fa";
+	private static final String Share_IranApps = "http://iranapps.ir/app/ir.hatamiarash.hyperonline";
+	private static final String Share_Myket = "http://myket.ir/app/ir.hatamiarash.hyperonline";
+	private static final String Share_Normal = "http://hyper-online.ir/HyperOnline.apk";
+	public static final String Share_App = Share_Myket;
+	
 }
