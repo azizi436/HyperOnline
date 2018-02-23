dfdddb32baa3b49ee33ad54e9bd6d0e8e36330fa
hatamiarash7 <hatamiarash7@gmail.com>
2018-02-24 00:39:22 +0330

    nothing
    Signed-off-by: hatamiarash7 <hatamiarash7@gmail.com>

--- app/src/main/java/ir/hatamiarash/utils/URLs.java
+++ app/src/main/java/ir/hatamiarash/utils/URLs.java
@@ -17,6 +17,6 @@ public class URLs {
 	private static final String Share_IranApps = "https://iranapps.ir/app/ir.hatamiarash.hyperonline";
 	private static final String Share_Myket = "https://myket.ir/app/ir.hatamiarash.hyperonline";
 	private static final String Share_Normal = "https://hyper-online.ir/HyperOnline.apk";
-	public static final String Share_App = Share_Myket;
+	public static final String Share_App = Share_Normal;
 	
 }
