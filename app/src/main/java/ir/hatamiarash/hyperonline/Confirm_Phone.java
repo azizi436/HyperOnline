c26621a5fcb7a8d2248094ef4c9fb395df57d425
hatamiarash7 <hatamiarash7@gmail.com>
2018-04-23 20:02:08 +0430

    add logout button change call button

--- app/src/main/java/ir/hatamiarash/hyperonline/Confirm_Phone.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/Confirm_Phone.java
@@ -15,6 +15,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -58,15 +59,26 @@ import butterknife.ButterKnife;
 import helper.ConfirmManager;
 import helper.FontHelper;
 import helper.Helper;
+import helper.SQLiteHandler;
+import helper.SQLiteHandlerItem;
+import helper.SQLiteHandlerSupport;
+import helper.SessionManager;
 import ir.hatamiarash.interfaces.SmsListener;
 import ir.hatamiarash.receivers.SmsReceiver;
 import ir.hatamiarash.utils.TAGs;
 import ir.hatamiarash.utils.Values;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class Confirm_Phone extends AppCompatActivity implements SmsListener {
 	private SweetAlertDialog progressDialog;
 	private ConfirmManager confirmManager;
-	SmsReceiver receiver;
+	private SQLiteHandler db_user;
+	private SQLiteHandlerItem db_item;
+	private SQLiteHandlerSupport db_support;
+	private SessionManager session;
+	private SmsReceiver receiver;
+	private Vibrator vibrator;
+	
 	
 	@BindView(R.id.button0)
 	Button button0;
@@ -93,7 +105,9 @@ public class Confirm_Phone extends AppCompatActivity implements SmsListener {
 	@BindView(R.id.time)
 	TextView time;
 	@BindView(R.id.help)
-	TextView help;
+	FancyButton help;
+	@BindView(R.id.logout)
+	FancyButton logout;
 	@BindView(R.id.phone)
 	TextView phone;
 	
@@ -142,6 +156,11 @@ public class Confirm_Phone extends AppCompatActivity implements SmsListener {
 		progressDialog.setCancelable(false);
 		progressDialog.getProgressHelper().setBarColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
 		progressDialog.setTitleText(getResources().getString(R.string.wait));
+		session = new SessionManager(getApplicationContext());
+		vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+		db_user = new SQLiteHandler(getApplicationContext());
+		db_item = new SQLiteHandlerItem(getApplicationContext());
+		db_support = new SQLiteHandlerSupport(getApplicationContext());
 		
 		phoneNumber = getIntent().getStringExtra(TAGs.PHONE);
 		phone.setText(phoneNumber);
@@ -185,17 +204,41 @@ public class Confirm_Phone extends AppCompatActivity implements SmsListener {
 		
 		time.setVisibility(View.VISIBLE);
 		help.setVisibility(View.GONE);
+		logout.setVisibility(View.GONE);
 		
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
 			getPermission(Confirm_Phone.this);
 		else
 			RequestCode();
 		
+		help.setCustomTextFont("sans.ttf");
+		logout.setCustomTextFont("sans.ttf");
+		
 		help.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent(Intent.ACTION_DIAL);
 				intent.setData(Uri.parse("tel:" + Values.phoneNumber));
 				startActivity(intent);
+			}
+		});
+		
+		logout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				vibrator.vibrate(50);
+				showDialog();
+				session.setLogin(false);
+				confirmManager.setPhoneConfirm(false);
+				confirmManager.setInfoConfirm(false);
+				db_user.deleteUsers();
+				db_item.deleteItems();
+				db_support.deleteMessages();
+				hideDialog();
+				Helper.MakeToast(getApplicationContext(), "با موفقیت خارج شدید", TAGs.SUCCESS);
+				Intent i = new Intent(getApplicationContext(), Activity_Main.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(i);
+				finish();
 			}
 		});
 	}
@@ -213,6 +256,7 @@ public class Confirm_Phone extends AppCompatActivity implements SmsListener {
 			
 			public void onFinish() {
 				help.setVisibility(View.VISIBLE);
+				logout.setVisibility(View.VISIBLE);
 				time.setText("کد فعالسازی ارسال شده است. در صورت عدم دریافت کد ، پس از بررسی وضعیت تلفن همراه خود با ما در تماس باشید.");
 				time.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
 			}
