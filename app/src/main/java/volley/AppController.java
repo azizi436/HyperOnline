001bc5a42b66ac8656d31359bc6090fe4c52df80
hatamiarash7 <hatamiarash7@gmail.com>
2018-04-27 20:23:22 +0430

    add annotation

--- app/src/main/java/volley/AppController.java
+++ app/src/main/java/volley/AppController.java
@@ -12,12 +12,15 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
+import org.jetbrains.annotations.Contract;
+
 public class AppController extends Application {
 	private static final String TAG = AppController.class.getSimpleName();
 	private static AppController mInstance;
 	private RequestQueue mRequestQueue;
 	private ImageLoader mImageLoader;
 	
+	@Contract(pure = true)
 	public static synchronized AppController getInstance() {
 		return mInstance;
 	}
