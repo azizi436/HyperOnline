07637202213a888c38a3c590d6d4b4cf525111e6
hatamiarash7 <hatamiarash7@gmail.com>
2018-06-02 21:49:25 +0430

    complete transfer confirmation

--- app/src/main/java/ir/hatamiarash/hyperonline/activities/Activity_TransferConfirm.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/activities/Activity_TransferConfirm.java
@@ -1,12 +1,15 @@
 package ir.hatamiarash.hyperonline.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -15,18 +18,33 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.crashlytics.android.Crashlytics;
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+
+import org.jetbrains.annotations.Contract;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ir.hatamiarash.hyperonline.HyperOnline;
 import ir.hatamiarash.hyperonline.R;
 import ir.hatamiarash.hyperonline.helpers.FontHelper;
+import ir.hatamiarash.hyperonline.helpers.Helper;
 import ir.hatamiarash.hyperonline.interfaces.Analytics;
 import ir.hatamiarash.hyperonline.utils.TAGs;
+import timber.log.Timber;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
+import static ir.hatamiarash.hyperonline.HyperOnline.HOST;
+import static ir.hatamiarash.hyperonline.helpers.FormatHelper.fixResponse;
 import static ir.hatamiarash.hyperonline.helpers.PriceHelper.formatPrice;
 
 public class Activity_TransferConfirm extends AppCompatActivity {
@@ -35,6 +53,9 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 	HyperOnline application;
 	Analytics analytics;
 	SweetAlertDialog progressDialog;
+	Vibrator vibrator;
+	Response.Listener<String> listener;
+	Response.ErrorListener errorListener;
 	
 	@BindView(R.id.toolbar)
 	Toolbar toolbar;
@@ -51,9 +72,10 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 	@BindView(R.id.message)
 	TextView message;
 	@BindView(R.id.confirm)
-	CardView confirm;
+	TextView confirm;
 	
-	String transferCode;
+	String transferDest;
+	String transferSrc;
 	String transferUser;
 	String transferPrice;
 	String currentPrice;
@@ -70,6 +92,7 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 		progressDialog.setCancelable(false);
 		progressDialog.getProgressHelper().setBarColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
 		progressDialog.setTitleText(getResources().getString(R.string.wait));
+		vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
 		
 		setSupportActionBar(toolbar);
 		try {
@@ -92,7 +115,8 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 			getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 		}
 		
-		transferCode = getIntent().getStringExtra(TAGs.CODE);
+		transferDest = getIntent().getStringExtra(TAGs.CODE);
+		transferSrc = getIntent().getStringExtra("src_code");
 		transferUser = getIntent().getStringExtra(TAGs.NAME);
 		transferPrice = getIntent().getStringExtra("tPrice");
 		currentPrice = getIntent().getStringExtra(TAGs.PRICE);
@@ -100,7 +124,7 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 		message.setVisibility(View.GONE);
 		tPrice.setText(formatPrice(transferPrice) + " تومان");
 		cPrice.setText(formatPrice(currentPrice) + " تومان");
-		int afterPrice = Integer.valueOf(currentPrice) - Integer.valueOf(transferPrice);
+		final int afterPrice = Integer.valueOf(currentPrice) - Integer.valueOf(transferPrice);
 		aPrice.setText(formatPrice(String.valueOf(afterPrice)) + " تومان");
 		cWallet.setText("اصلی");
 		tUser.setText(transferUser);
@@ -110,7 +134,101 @@ public class Activity_TransferConfirm extends AppCompatActivity {
 			confirm.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.gray3));
 		}
 		
+		confirm.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				vibrator.vibrate(50);
+				if (afterPrice >= 0) {
+					transferMoney(transferSrc, transferDest, getIntent().getStringExtra(TAGs.UID), transferPrice);
+				} else {
+					Helper.MakeToast(getApplicationContext(), "موجودی کافی نیست", TAGs.ERROR);
+				}
+			}
+		});
+		
+		listener = new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				hideDialog();
+				try {
+					response = fixResponse(response);
+					Timber.tag(CLASS).d(response);
+					JSONObject object = new JSONObject(response);
+					Boolean error = object.getBoolean(TAGs.ERROR);
+					if (!error) {
+						Intent intent = new Intent(getApplicationContext(), Activity_Main.class);
+						intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						startActivity(intent);
+						startActivity(new Intent(getApplicationContext(), Activity_Transactions.class));
+						Helper.MakeToast(getApplicationContext(), "انتقال موفقیت آمیز بود", TAGs.SUCCESS);
+						finish();
+					} else {
+						String errorMsg = object.getString(TAGs.ERROR_MSG);
+						Helper.MakeToast(getApplicationContext(), errorMsg, TAGs.ERROR);
+						finish();
+					}
+				} catch (JSONException e) {
+					Crashlytics.logException(e);
+					hideDialog();
+					finish();
+				}
+			}
+		};
+		
+		errorListener = new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Crashlytics.logException(error);
+				hideDialog();
+				finish();
+			}
+		};
+		
 		analyticsReport();
+	}
+	
+	private void transferMoney(String src_code, String des_code, String user_id, String price) {
+		showDialog();
+		try {
+			String URL = getResources().getString(R.string.url_api, HOST) + "transferMoney";
+			JSONObject params = new JSONObject();
+			params.put("dest_code", des_code);
+			params.put("src_code", src_code);
+			params.put("user_id", user_id);
+			params.put("price", price);
+			final String mRequestBody = params.toString();
+			
+			StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, listener, errorListener) {
+				@NonNull
+				@Contract(pure = true)
+				@Override
+				public String getBodyContentType() {
+					return "application/json; charset=utf-8";
+				}
+				
+				@Nullable
+				@Override
+				public byte[] getBody() {
+					try {
+						return mRequestBody.getBytes("utf-8");
+					} catch (UnsupportedEncodingException e) {
+						Crashlytics.logException(e);
+						hideDialog();
+						return null;
+					}
+				}
+			};
+			// add retry policy to prevent send request twice
+			stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+					0,
+					DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+					DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+			));
+			application.addToRequestQueue(stringRequest);
+		} catch (JSONException e) {
+			Crashlytics.logException(e);
+			hideDialog();
+		}
 	}
 	
 	private void showDialog() {
