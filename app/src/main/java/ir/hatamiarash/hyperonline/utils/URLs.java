0e230f1666bec84649575f46052384791b3dc911
hatamiarash7 <hatamiarash7@gmail.com>
2018-06-09 16:00:54 +0430

    enhance urls

--- app/src/main/java/ir/hatamiarash/hyperonline/utils/URLs.java
+++ app/src/main/java/ir/hatamiarash/hyperonline/utils/URLs.java
@@ -8,12 +8,8 @@ public class URLs {
 	private static final String Share_CafeBazaar = "https://cafebazaar.ir/app/ir.hatamiarash.hyperonline/?l=fa";
 	private static final String Share_IranApps = "http://iranapps.ir/app/ir.hatamiarash.hyperonline";
 	private static final String Share_Myket = "https://myket.ir/app/ir.hatamiarash.hyperonline";
-	private static final String Share_Normal = "https://hyper-online.ir/HyperOnline.apk";
-	public static final String Share_App = Share_Normal;
+	private static final String Share_Normal = "https://t.me/online_hyper";
 	
-	private static final String Update_CafeBazzar = Share_CafeBazaar;
-	private static final String Update_IranApps = Share_IranApps;
-	private static final String Update_Myket = Share_Myket;
-	private static final String Update_Normal = "https://t.me/online_hyper";
-	public static final String Update_App = Update_CafeBazzar;
+	public static final String Share_App = Share_Normal;
+	public static final String Update_App = Share_App;
 }
